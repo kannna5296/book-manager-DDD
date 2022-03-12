ec47836ebd601d8159da5b67422f573be8d3b90a
kannna5296 <kannnahacks5296@gmail.com>
2022-03-12 22:24:34 +0900

    Update追加

--- src/main/kotlin/com/book/manager/bookmanager/infra/database/mapper/BookMapper.kt
+++ src/main/kotlin/com/book/manager/bookmanager/infra/database/mapper/BookMapper.kt
@@ -12,11 +12,11 @@ interface BookMapper {
     @Select("SELECT id, title, author, release_date FROM book WHERE id = #{id}")
     fun select(id: Long): BookRecord
 
-    @Insert("INSERT INTO book (title, author, release_date) VALUES (#{title},#{author},#{release_date})")
+    @Insert("INSERT INTO book (id, title, author, release_date) VALUES (#{id}, #{title},#{author},#{releaseDate})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     fun insert(model: BookRecord)
 
-    @Update("UPDATE book SET title = #{title}, author = #{author}, release_date = #{release} WHERE id = #{id}")
+    @Update("UPDATE book SET title = #{title}, author = #{author}, release_date = #{releaseDate} WHERE id = #{id}")
     fun updateByPrimaryKeySelective(model: BookRecord)
 
     @Delete("DELETE FROM book WHERE id = #{id}")
