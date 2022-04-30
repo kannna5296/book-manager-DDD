10f0872f3d0bc99310a290836cb0959d5aeedb32
kannna5296 <kannnahacks5296@gmail.com>
2022-04-30 10:28:36 +0900

    project追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "book-manager"
+include(":usecase")
 include(":domain")
---
+++ usecase/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    //cleanアーキテクチャの考え方に寄り、usecase層はdomain層にのみ依存する
+    //application層ともいう
+    implementation(project(":domain"))
+}
