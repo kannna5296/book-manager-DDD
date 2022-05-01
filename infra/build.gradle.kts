a4aaebfa0634921f7b6ea035466c074d0aed8c64
kannna5296 <kannnahacks5296@gmail.com>
2022-05-01 13:23:04 +0900

    package修正

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     //infraはドメインに依存
-    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.1")
     // https://mvnrepository.com/artifact/com.h2database/h2
     implementation("com.h2database:h2:1.3.148")
     runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
