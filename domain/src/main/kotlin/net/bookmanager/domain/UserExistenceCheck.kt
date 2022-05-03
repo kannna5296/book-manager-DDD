3b8091a22993e6cebc10cf5d0ae9a84a8ab0b86f
kannna5296 <kannnahacks5296@gmail.com>
2022-05-03 09:13:47 +0900

    ユーザの存在チェック追加（一応外側だけ）

---
+++ domain/src/main/kotlin/net/bookmanager/domain/UserExistenceCheck.kt
@@ -0,0 +1,15 @@
+package net.bookmanager.domain
+
+import net.bookmanager.domain.book.IBookRepository
+
+class UserExistenceCheck(
+
+    //TODO IUerRepositoryなど実装
+
+    //private val bookRepository: IBookRepository
+    ) {
+
+//    fun exists(id: Int) {
+//        val book = bookRepository.findById(id) ?: throw IllegalStateException("")
+//    }
+}
--- domain/src/main/kotlin/net/bookmanager/domain/rental/UserId.kt
+++ domain/src/main/kotlin/net/bookmanager/domain/rental/UserId.kt
@@ -3,8 +3,4 @@ package net.bookmanager.domain.rental
 import net.bookmanager.domain.ValueObject
 
 class UserId(val value:Int):ValueObject {
-    //TODO 存在チェック
-    init {
-
-    }
 }
