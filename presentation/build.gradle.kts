bc816502b321b2b0d08013ed5d848f75cf1d0b3a
kannna5296 <kannnahacks5296@gmail.com>
2022-05-05 14:59:36 +0900

    presentation層Dependency整理

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
-    // 全部要る
+    // usecaseに依存
     implementation(project(":usecase"))
+    // infraに依存(mapperscanのため)
+    implementation(project(":infra"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.1")
 }
