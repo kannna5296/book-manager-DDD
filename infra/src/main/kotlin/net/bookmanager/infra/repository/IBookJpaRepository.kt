825cd33d7dafa6bf12c21b09c2ca31d2551eb230
kannna5296 <kannnahacks5296@gmail.com>
2022-05-05 17:31:36 +0900

    修正

--- infra/src/main/kotlin/net/bookmanager/infra/repository/IBookJpaRepository.kt
+++ infra/src/main/kotlin/net/bookmanager/infra/repository/IBookJpaRepository.kt
@@ -1,10 +1,11 @@
 package net.bookmanager.infra.repository
 
+import net.bookmanager.domain.book.IBookRepository
 import net.bookmanager.infra.entity.BookEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface IBookJpaRepository : JpaRepository<BookEntity, Int?> {
+interface IBookJpaRepository : JpaRepository<BookEntity, Int?> , IBookRepository{
     // TODO JPAデフォルト提供以外に都度必要なメソッド追加
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     // usecaseに依存
     implementation(project(":usecase"))
     // infraに依存(mapperscanのため)
-    implementation(project(":infra"))
+    runtimeOnly(project(":infra"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     // implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.1")
     implementation("org.springframework.data:spring-data-jpa:2.6.1")
